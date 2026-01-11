e49c921f93530fa6856d82592f0cb78ad4201586
Robyn Mac <scuadra02@gmail.com>
2026-01-11 18:02:22 -0300

    feat: css basic rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-5xl m-auto`}
       >
+  
         {children}
       </body>
     </html>
