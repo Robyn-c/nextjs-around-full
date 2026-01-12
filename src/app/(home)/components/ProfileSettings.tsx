fe3bca912106a48d91a4a987a8f0e9143a057256
Robyn Mac <scuadra02@gmail.com>
2026-01-12 16:19:43 -0300

    fix: redundant css classname

--- src/app/(home)/components/ProfileSettings.tsx
+++ src/app/(home)/components/ProfileSettings.tsx
@@ -7,7 +7,7 @@ function ProfileSettings() {
         <Image src="/profile-pic.jpg" alt="Image depicting explorer Jacques Cousteau" width={120} height={120}
         className="max-w-1/4 rounded-full"
         />
-        <hgroup className="items-center">
+        <hgroup>
           <h1 className="text-2xl font-semibold">Jacques Cousteau</h1>
           <p className="text-md">Explorer</p>
         </hgroup>
